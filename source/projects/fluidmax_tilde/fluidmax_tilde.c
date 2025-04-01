eb1ce3619521a5614546968e812f7d0671358a51
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-04-01 04:42:10 +0300

    compiles no errors

--- source/projects/fluidmax_tilde/fluidmax_tilde.c
+++ source/projects/fluidmax_tilde/fluidmax_tilde.c
@@ -1095,6 +1095,89 @@ void fluidmax_version(t_object* o)
 
 // extern fluid_gen_info_t fluid_gen_info[];
 
+typedef struct _fluid_gen_info_t
+{
+    char num;       /* Generator number */
+    char *name;
+    char init;      /* Does the generator need to be initialized (not used) */
+    char nrpn_scale;    /* The scale to convert from NRPN (cfr. fluid_gen_map_nrpn()) */
+    float min;      /* The minimum value */
+    float max;      /* The maximum value */
+    float def;      /* The default value (cfr. fluid_gen_init()) */
+} fluid_gen_info_t;
+
+#define _GEN(_name) GEN_ ## _name, #_name
+
+/* See SFSpec21 $8.1.3 */
+static const fluid_gen_info_t fluid_gen_info[] =
+{
+    /* number/name             init  nrpn-scale    min        max         def */
+    { _GEN(STARTADDROFS),           1,     1,       0.0f,     1e10f,       0.0f },
+    { _GEN(ENDADDROFS),             1,     1,     -1e10f,      0.0f,       0.0f },
+    { _GEN(STARTLOOPADDROFS),       1,     1,     -1e10f,     1e10f,       0.0f },
+    { _GEN(ENDLOOPADDROFS),         1,     1,     -1e10f,     1e10f,       0.0f },
+    { _GEN(STARTADDRCOARSEOFS),     0,     1,       0.0f,     1e10f,       0.0f },
+    { _GEN(MODLFOTOPITCH),          1,     2,  -12000.0f,  12000.0f,       0.0f },
+    { _GEN(VIBLFOTOPITCH),          1,     2,  -12000.0f,  12000.0f,       0.0f },
+    { _GEN(MODENVTOPITCH),          1,     2,  -12000.0f,  12000.0f,       0.0f },
+    { _GEN(FILTERFC),               1,     2,    1500.0f,  13500.0f,   13500.0f },
+    { _GEN(FILTERQ),                1,     1,       0.0f,    960.0f,       0.0f },
+    { _GEN(MODLFOTOFILTERFC),       1,     2,  -12000.0f,  12000.0f,       0.0f },
+    { _GEN(MODENVTOFILTERFC),       1,     2,  -12000.0f,  12000.0f,       0.0f },
+    { _GEN(ENDADDRCOARSEOFS),       0,     1,     -1e10f,      0.0f,       0.0f },
+    { _GEN(MODLFOTOVOL),            1,     1,    -960.0f,    960.0f,       0.0f },
+    { _GEN(UNUSED1),                0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(CHORUSSEND),             1,     1,       0.0f,   1000.0f,       0.0f },
+    { _GEN(REVERBSEND),             1,     1,       0.0f,   1000.0f,       0.0f },
+    { _GEN(PAN),                    1,     1,    -500.0f,    500.0f,       0.0f },
+    { _GEN(UNUSED2),                0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(UNUSED3),                0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(UNUSED4),                0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(MODLFODELAY),            1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(MODLFOFREQ),             1,     4,  -16000.0f,   4500.0f,       0.0f },
+    { _GEN(VIBLFODELAY),            1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(VIBLFOFREQ),             1,     4,  -16000.0f,   4500.0f,       0.0f },
+    { _GEN(MODENVDELAY),            1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(MODENVATTACK),           1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(MODENVHOLD),             1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(MODENVDECAY),            1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(MODENVSUSTAIN),          0,     1,       0.0f,   1000.0f,       0.0f },
+    { _GEN(MODENVRELEASE),          1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(KEYTOMODENVHOLD),        0,     1,   -1200.0f,   1200.0f,       0.0f },
+    { _GEN(KEYTOMODENVDECAY),       0,     1,   -1200.0f,   1200.0f,       0.0f },
+    { _GEN(VOLENVDELAY),            1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(VOLENVATTACK),           1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(VOLENVHOLD),             1,     2,  -12000.0f,   5000.0f,  -12000.0f },
+    { _GEN(VOLENVDECAY),            1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(VOLENVSUSTAIN),          0,     1,       0.0f,   1440.0f,       0.0f },
+    { _GEN(VOLENVRELEASE),          1,     2,  -12000.0f,   8000.0f,  -12000.0f },
+    { _GEN(KEYTOVOLENVHOLD),        0,     1,   -1200.0f,   1200.0f,       0.0f },
+    { _GEN(KEYTOVOLENVDECAY),       0,     1,   -1200.0f,   1200.0f,       0.0f },
+    { _GEN(INSTRUMENT),             0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(RESERVED1),              0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(KEYRANGE),               0,     0,       0.0f,    127.0f,       0.0f },
+    { _GEN(VELRANGE),               0,     0,       0.0f,    127.0f,       0.0f },
+    { _GEN(STARTLOOPADDRCOARSEOFS), 0,     1,     -1e10f,     1e10f,       0.0f },
+    { _GEN(KEYNUM),                 1,     0,       0.0f,    127.0f,      -1.0f },
+    { _GEN(VELOCITY),               1,     1,       0.0f,    127.0f,      -1.0f },
+    { _GEN(ATTENUATION),            1,     1,       0.0f,   1440.0f,       0.0f },
+    { _GEN(RESERVED2),              0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(ENDLOOPADDRCOARSEOFS),   0,     1,     -1e10f,     1e10f,       0.0f },
+    { _GEN(COARSETUNE),             0,     1,    -120.0f,    120.0f,       0.0f },
+    { _GEN(FINETUNE),               0,     1,     -99.0f,     99.0f,       0.0f },
+    { _GEN(SAMPLEID),               0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(SAMPLEMODE),             0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(RESERVED3),              0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(SCALETUNE),              0,     1,       0.0f,   1200.0f,     100.0f },
+    { _GEN(EXCLUSIVECLASS),         0,     0,       0.0f,      0.0f,       0.0f },
+    { _GEN(OVERRIDEROOTKEY),        1,     0,       0.0f,    127.0f,      -1.0f },
+    { _GEN(PITCH),                  1,     0,       0.0f,    127.0f,       0.0f },
+    { _GEN(CUSTOM_BALANCE),         1,     0,    -960.0f,    960.0f,       0.0f },
+    { _GEN(CUSTOM_FILTERFC),        1,     2,       0.0f,  22050.0f,       0.0f },
+    { _GEN(CUSTOM_FILTERQ),         1,     1,       0.0f,    960.0f,       0.0f }
+};
+
+
 void fluidmax_print_soundfonts(t_fluidmax* x, t_symbol* s, short argc,
                                t_atom* argv)
 {
@@ -1219,34 +1302,32 @@ void fluidmax_print_channels(t_fluidmax* x, t_symbol* s, short argc,
     }
 }
 
-void fluidmax_print_generators(t_fluidmax* x, t_symbol* s, short argc,
-                               t_atom* argv)
+void fluidmax_print_generators(t_fluidmax* x, t_symbol* s, short argc, t_atom* argv)
 {
-    // int channel = 1;
-    // int n = GEN_LAST;
-    // int i;
+    int channel = 1;
+    int n = GEN_LAST;
+    int i;
 
-    // if (argc > 1 && is_number(argv + 1))
-    //     channel = get_number_as_long(argv + 1);
+    if (argc > 1 && is_number(argv + 1))
+        channel = get_number_as_long(argv + 1);
 
-    // if (channel < 1)
-    //     channel = 1;
-    // else if (channel > fluid_synth_count_midi_channels(x->synth))
-    //     channel = fluid_synth_count_midi_channels(x->synth);
+    if (channel < 1)
+        channel = 1;
+    else if (channel > fluid_synth_count_midi_channels(x->synth))
+        channel = fluid_synth_count_midi_channels(x->synth);
 
-    // post("fluidsynth~ generators of channel %d:", channel);
+    post("fluidsynth~ generators of channel %d:", channel);
 
-    // for (i = 0; i < n; i++) {
-    //     const char* name = fluidmax_gen_info[i].name;
-    //     const char* unit = fluidmax_gen_info[i].unit;
-    //     double incr = fluid_synth_get_gen(x->synth, channel - 1,
-    //                                       i);
-    //     double min = fluid_gen_info[i].min;
-    //     double max = fluid_gen_info[i].max;
+    for (i = 0; i < n; i++) {
+        const char* name = fluidmax_gen_info[i].name;
+        const char* unit = fluidmax_gen_info[i].unit;
+        double incr = fluid_synth_get_gen(x->synth, channel - 1, i);
+        double min = fluid_gen_info[i].min;
+        double max = fluid_gen_info[i].max;
 
-    //     post("  %d '%s': %s %g [%g ... %g] (%s)", i, name,
-    //          (incr >= 0) ? "" : "-", fabs(incr), min, max, unit);
-    // }
+        post("  %d '%s': %s %g [%g ... %g] (%s)", i, name,
+             (incr >= 0) ? "" : "-", fabs(incr), min, max, unit);
+    }
 }
 
 void fluidmax_print_gain(t_fluidmax* x, t_symbol* s, short argc, t_atom* argv)
